cd2917237ee751983fd87f878f7b133c5ebd1924
f1dao <f1dao@qq.com>
2022-06-25 19:27:37 +0800

    chore: 🔨 修改配置

---
+++ .commitlintrc.js
@@ -0,0 +1,49 @@
+// .commitlintrc.js
+/** @type {import('cz-git').UserConfig} */
+const { execSync } = require('child_process');
+
+// @tip: git branch name = feature/issue_33   =>    auto get defaultIssues = #33
+ const issue = execSync('git rev-parse --abbrev-ref HEAD')
+  .toString()
+  .trim()
+  .split("_")[1]
+
+module.exports = {
+  rules: {
+    // @see: https://commitlint.js.org/#/reference-rules
+  },
+  prompt: {
+    messages: {
+      type: "选择你要提交的类型 :",
+      scope: "选择一个提交范围（可选）:",
+      customScope: "请输入自定义的提交范围 :",
+      subject: "填写简短精炼的变更描述 :\n",
+      body: '填写更加详细的变更描述（可选）。使用 "|" 换行 :\n',
+      breaking: '列举非兼容性重大的变更（可选）。使用 "|" 换行 :\n',
+      footerPrefixsSelect: "选择关联issue前缀（可选）:",
+      customFooterPrefixs: "输入自定义issue前缀 :",
+      footer: "列举关联issue (可选) 例如: #31, #I3244 :\n",
+      confirmCommit: "是否提交或修改commit ?"
+    },
+    types: [
+      {value: 'feat',     name: 'feat:     ✨ 新增功能 | A new feature', emoji: '✨'},
+      {value: 'fix',      name: 'fix:      🐛 修复缺陷 | A bug fix', emoji: '🐛'},
+      {value: 'docs',     name: 'docs:     📝 文档更新 | Documentation only changes', emoji: ':memo:'},
+      {value: 'style',    name: 'style:    💄 代码格式 | Changes that do not affect the meaning of the code', emoji: '💄'},
+      {value: 'refactor', name: 'refactor: ♻️ 代码重构 | A code change that neither fixes a bug nor adds a feature', emoji: '♻️'},
+      {value: 'perf',     name: 'perf:     ⚡️ 性能提升 | A code change that improves performance', emoji: '⚡️'},
+      {value: 'test',     name: 'test:     ✅ 测试相关 | Adding missing tests or correcting existing tests', emoji: '✅'},
+      {value: 'build',    name: 'build:    📦️ 构建相关 | Changes that affect the build system or external dependencies', emoji: '📦️'},
+      {value: 'ci',       name: 'ci:       🎡 持续集成 | Changes to our CI configuration files and scripts', emoji: '🎡'},
+      {value: 'revert',   name: 'revert:   ⏪️ 回退代码 | Revert to a commit', emoji: '⏪️'},
+      {value: 'chore',    name: 'chore:    🔨 其他修改 | Other changes that do not modify src or test files', emoji: '🔨'},
+    ],
+    useEmoji: true,
+    issuePrefixs: [
+      // 如果使用 gitee 作为开发管理
+      { value: 'link', name: 'link:     链接 ISSUES 进行中'},
+      { value: 'comment', name: 'comment: 评论 ISSUES'},
+      { value: 'closed', name: 'closed:   标记 ISSUES 已完成' }
+    ],
+  }
+};
