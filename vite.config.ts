a97a3e6a39cb5579a645dfd406396bbc3c980e53
readpage <f1dao@qq.com>
2022-08-09 14:55:33 +0800

    chore: 🔨 😣

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import DefineOptions from 'unplugin-vue-define-options/vite'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
+//
 export default defineConfig({
   plugins: [DefineOptions(), vue()],
   resolve: {
