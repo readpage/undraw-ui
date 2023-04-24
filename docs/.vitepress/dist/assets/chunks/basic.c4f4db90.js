42e031b476773eebd7e27ea98aba69ea9d0434d0
readpage <f1dao@qq.com>
2023-04-24 21:49:17 +0800

    other: .gitignore

---
+++ docs/.vitepress/dist/assets/chunks/basic.c4f4db90.js
@@ -0,0 +1,1 @@
+import{d as o,G as n,o as t,A as a}from"./framework.347a35cb.js";const u=o({__name:"basic",setup(c){return(r,s)=>{const e=n("u-counter");return t(),a(e,{"start-amount":500,"end-amount":1e4})}}});export{u as default};
