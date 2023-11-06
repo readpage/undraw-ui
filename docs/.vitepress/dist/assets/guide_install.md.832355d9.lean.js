b1964bde0da0b25d11489cdde249077c74dc6642
readpage <f1dao@qq.com>
2023-11-06 21:29:40 +0800

    docs: 📄 发布v1.0.0版本

---
+++ docs/.vitepress/dist/assets/guide_install.md.832355d9.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,i as e,j as s,O as t}from"./chunks/framework.f5822349.js";const u=JSON.parse('{"title":"安装","description":"","frontmatter":{},"headers":[],"relativePath":"guide/install.md","lastUpdated":1697501365000}'),n={name:"guide/install.md"},o=t("",4),l=[o];function p(c,r,i,d,_,h){return e(),s("div",null,l)}const C=a(n,[["render",p]]);export{u as __pageData,C as default};
